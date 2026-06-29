8cd591303645d5e202f85c1871ee2ad660ef503f
Nivas Kumar S <nivask457@gmail.com>
2026-06-29 18:22:18 +0530

    File Updated

--- index.php
+++ index.php
@@ -1,1 +1,183 @@
+<?php include "function.php" ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jagannath Vidyalaya Matriculation Higher Secondary School</title>
+    <link rel="stylesheet" href="Assets/CSS/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="Assets/Images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="Assets/Images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="Assets/Images/favicon-16x16.png">
+<link rel="manifest" href="Assets/Images/site.webmanifest">
+
+</head>
+
+<body>
+    <?=template_header()?>
+    <section class="hero-section">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Assets/Images/slider-1.webp" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Assets/Images/slider-2.webp" class="d-block w-100" alt="...">
+    </div>
+    <!-- <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div> -->
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </section>
+    <section class="home-2 py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <img src="Assets/Images/about-img.webp" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 col-lg-6">
+                    <span class="badge"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFC0A"><path d="M281.5-281.5Q200-363 200-480t81.5-198.5Q363-760 480-760t198.5 81.5Q760-597 760-480t-81.5 198.5Q597-200 480-200t-198.5-81.5Z"/></svg>About Us</span>
+                    <h2 class="section-title">Where Learning Inspires Success</h2>
+                    <div id="content">
+                        <p class="section-desc">At Jagannath Vidyalaya Matriculation Higher Secondary School, Thandalam, Thiruporur, we believe every child has the potential to achieve excellence. Our school provides a safe, inspiring, and supportive learning environment where students develop academic knowledge, strong values, creativity, confidence, and essential life skills to succeed in a rapidly changing world.</p>
+                    <p class="section-desc">Since our establishment in 2022, Jagannath Vidyalaya has been committed to nurturing intellectual curiosity, critical thinking, and a lifelong passion for learning. Our well-structured academic programs inspire students to achieve excellence, build confidence, strengthen problem-solving skills, and prepare them for higher education and future career success.</p>
+                    <a href="about.php" class="btn">More About Us</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="home-3 py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="card">
+                        <img src="Assets/Images/our-mission.webp" alt="" class="img-fluid">
+                        <div class="card-heading">
+                            <h3 class="card-title">Our Mission</h3>
+                            <p class="card-desc">To provide holistic education that fosters academic excellence, strong values, integrity, respect, inclusivity, and the skills and confidence needed to succeed in the 21st century.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="card">
+                        <img src="Assets/Images/vision.webp" alt="" class="img-fluid">
+                        <div class="card-heading">
+                            <h3 class="card-title">Our Vision</h3>
+                            <p class="card-desc">To cultivate confident leaders who inspire positive change, embrace innovation, and create a lasting impact in their communities while contributing meaningfully to a better world.</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="card">
+                        <img src="Assets/Images/core.webp" alt="" class="img-fluid">
+                        <div class="card-heading">
+                            <h3 class="card-title">Core Values</h3>
+                            <p class="card-desc">We uphold integrity, excellence, discipline, diversity, creativity, collaboration, empathy, and continuous growth to shape responsible, confident, and compassionate future leaders.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="home-4 py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <span class="badge"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFC0A"><path d="M281.5-281.5Q200-363 200-480t81.5-198.5Q363-760 480-760t198.5 81.5Q760-597 760-480t-81.5 198.5Q597-200 480-200t-198.5-81.5Z"/></svg>Academics</span>
+                    <h2 class="section-title">Shaping Bright Academic Minds</h2>
+                </div> 
+                <div class="col-12 col-md-6 col-lg-6">
+                    <p class="section-desc">At Jagannath Vidyalaya MHSS, we offer a structured and progressive curriculum designed to build a strong academic foundation while fostering intellectual and personal growth. Our course structure ensures conceptual clarity,critical thinking, and skill development at every level.</p>
+                    <a href="academics.php" class="btn">Read More</a>
+                </div>   
+            </div>
+            
+            <div class="row mt-4">
+                <div class="col-12 col-md-4 col-lg-4">
+                    <img src="Assets/Images/academic.webp" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-8 col-lg-8">
+                    <div class="accordion-group">
+                        <details class="acc-item">
+                            <summary>
+                            <span>Pre-Primary</span>
+                            <span class="acc-icon">+</span>
+                            </summary>
+                            <div class="content-wrapper">
+                            <div class="content-body">
+                                Content Section 1. The rusty swing set creaked a lonely lullaby in the twilight, shadows lengthening like grasping fingers across the dew-kissed grass. A lone dandelion seed, adrift on the breeze, whispered secrets of faraway fields, of dandelion suns and galaxies spun from fluff. Somewhere, beyond the veil of dusk, a coyote laughed, echoing through the stillness like a forgotten memory.
+                            </div>
+                            </div>
+                        </details>
+                        <details class="acc-item">
+                            <summary>
+                            <span>Primary</span>
+                            <span class="acc-icon">+</span>
+                            </summary>
+                            <div class="content-wrapper">
+                            <div class="content-body">
+                                Content Section 2. The rusty swing set creaked a lonely lullaby in the twilight, shadows lengthening like grasping fingers across the dew-kissed grass. A lone dandelion seed, adrift on the breeze, whispered secrets of faraway fields, of dandelion suns and galaxies spun from fluff. Somewhere, beyond the veil of dusk, a coyote laughed, echoing through the stillness like a forgotten memory.
+                            </div>
+                            </div>
+                        </details>
+                        <details class="acc-item">
+                            <summary>
+                            <span>Middle School</span>
+                            <span class="acc-icon">+</span>
+                            </summary>
+                            <div class="content-wrapper">
+                            <div class="content-body">
+                                Content Section 3. The rusty swing set creaked a lonely lullaby in the twilight, shadows lengthening like grasping fingers across the dew-kissed grass. A lone dandelion seed, adrift on the breeze, whispered secrets of faraway fields, of dandelion suns and galaxies spun from fluff. Somewhere, beyond the veil of dusk, a coyote laughed, echoing through the stillness like a forgotten memory.
+                            </div>
+                            </div>
+                        </details>
+                        <details class="acc-item">
+                            <summary>
+                            <span>High School</span>
+                            <span class="acc-icon">+</span>
+                            </summary>
+                            <div class="content-wrapper">
+                            <div class="content-body">
+                                Content Section 3. The rusty swing set creaked a lonely lullaby in the twilight, shadows lengthening like grasping fingers across the dew-kissed grass. A lone dandelion seed, adrift on the breeze, whispered secrets of faraway fields, of dandelion suns and galaxies spun from fluff. Somewhere, beyond the veil of dusk, a coyote laughed, echoing through the stillness like a forgotten memory.
+                            </div>
+                            </div>
+                        </details>
+                        <details class="acc-item">
+                            <summary>
+                            <span>Higher Secondary</span>
+                            <span class="acc-icon">+</span>
+                            </summary>
+                            <div class="content-wrapper">
+                            <div class="content-body">
+                                Content Section 3. The rusty swing set creaked a lonely lullaby in the twilight, shadows lengthening like grasping fingers across the dew-kissed grass. A lone dandelion seed, adrift on the breeze, whispered secrets of faraway fields, of dandelion suns and galaxies spun from fluff. Somewhere, beyond the veil of dusk, a coyote laughed, echoing through the stillness like a forgotten memory.
+                            </div>
+                            </div>
+                        </details>
+                        </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    <script src="Assets/JS/script.js"></script>
+</body>
+</html>
